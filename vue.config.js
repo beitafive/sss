2f1435434ca0ee10182479face0b99b21197ecae
wuzhaoqian <beitafive@163.com>
2019-10-10 16:56:00 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ const resolve = dir => {
 }
 
 module.exports = {
-    publicPath: '/sss/docs/',
+    publicPath: '/sss/',
     outputDir: 'docs',
     lintOnSave: true,
     chainWebpack: config => {
